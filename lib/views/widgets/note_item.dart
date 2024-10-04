462d00963622bda640ae98c70f98d6d874c49983
eslam <eslam.mohsen99@gmail.com>
2024-10-04 02:20:18 +0200

    Create edite note view.

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,48 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.deepPurpleAccent,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) {
+            return EditNoteView();
+          },
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.deepPurpleAccent,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: Text(
+                  'build your career with Eslam Mohsen',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.6), fontSize: 17),
+                ),
+              ),
+              trailing: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 15),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 6, right: 10, top: 20),
               child: Text(
-                'build your career with Eslam Mohsen',
+                'Oct28 , 1999',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.6), fontSize: 17),
+                  color: Colors.black.withOpacity(0.6),
+                ),
               ),
             ),
-            trailing: const Icon(
-              Icons.delete,
-              color: Colors.black,
-              size: 30,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 6, right: 10, top: 20),
-            child: Text(
-              'Oct28 , 1999',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.6),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
